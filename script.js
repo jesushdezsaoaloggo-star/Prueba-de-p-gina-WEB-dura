f043dee341d6b5c42e7f51a4607733a2285fc6c5
Menhera kun <jesushdezsaoaloggo@gmail.com>
2026-05-23 16:52:56 -0400

    Primera página web

---
+++ script.js
@@ -0,0 +1,3 @@
+function saludar() {
+    alert(Ti IP: 192.169.1.128. vives en Cuba, Holguín, vas a la Universidad Oscar Lucero Moya. No te asustes si te llegan a robar. AJAJAJSJSJSAJJASJSJDJAJ);
+}
